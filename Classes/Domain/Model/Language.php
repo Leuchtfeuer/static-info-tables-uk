77ec7f3f5ad68ec9f2514b9970161109ffa93523
Florian Wessels <f.wessels@bitmotion.de>
2020-02-06 19:44:55 +0100

    [TASK] Apply CS

--- Classes/Domain/Model/Language.php
+++ Classes/Domain/Model/Language.php
@@ -1,5 +1,5 @@
 <?php
-declare(strict_types=1);
+declare(strict_types = 1);
 namespace SJBR\StaticInfoTables\Domain\Model;
 
 class Language extends AbstractEntity
--- Classes/Extension.php
+++ Classes/Extension.php
@@ -1,6 +1,17 @@
 <?php
-declare(strict_types=1);
+declare(strict_types = 1);
 namespace Bitmotion\StaticInfoTablesUk;
+
+/***
+ *
+ * This file is part of the "Static Info Tables (UK)" Extension for TYPO3 CMS.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ *  (c) 2018 Florian Wessels <f.wessels@bitmotion.de>, Bitmotion GmbH
+ *
+ ***/
 
 class Extension
 {
