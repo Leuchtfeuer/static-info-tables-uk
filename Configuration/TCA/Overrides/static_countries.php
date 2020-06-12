dc1d7f31f6771d75d787f2b7d089f1f34b49d7a0
Florian Wessels <flo.wessels@gmail.com>
2020-06-12 11:52:16 +0200

    [TASK] Apply CS

--- Configuration/TCA/Overrides/static_countries.php
+++ Configuration/TCA/Overrides/static_countries.php
@@ -1,5 +1,16 @@
 <?php
-declare(strict_types = 1);
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the "Static Info Tables (UK)" extension for TYPO3 CMS.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * Florian Wessels <f.wessels@Leuchtfeuer.com>, Leuchtfeuer Digital Marketing
+ */
+
 defined('TYPO3_MODE') || die;
 
 call_user_func(
