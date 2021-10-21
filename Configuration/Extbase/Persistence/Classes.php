47f57131472821ae35bba3464bf66e152554c51a
Max Rösch <github@elbebass.com>
2021-10-21 10:49:35 +0200

    [BUGFIX] Fix typo in extbase persistance class

--- Configuration/Extbase/Persistence/Classes.php
+++ Configuration/Extbase/Persistence/Classes.php
@@ -8,7 +8,7 @@ declare(strict_types=1);
  * For the full copyright and license information, please read the
  * LICENSE.txt file that was distributed with this source code.
  *
- * Florian Wessels <f.wessels@Leuchtfeuer.com>, Leuchtfeuer Digital Marketing
+ * Max Rösch <m.roesch@Leuchtfeuer.com>, Leuchtfeuer Digital Marketing
  */
 
 return [
@@ -35,7 +35,7 @@ return [
         'tableName' => 'static_languages',
         'properties' => [
             'nameUk' => [
-                'fieldName' => 'cu_name_uk',
+                'fieldName' => 'lg_name_uk',
             ],
         ],
     ],
@@ -43,7 +43,7 @@ return [
         'tableName' => 'static_territories',
         'properties' => [
             'nameUk' => [
-                'fieldName' => 'cu_name_uk',
+                'fieldName' => 'tr_name_uk',
             ],
         ],
     ],
