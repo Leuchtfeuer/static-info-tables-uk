3d535b9f75ce7c8495240e9ee1919d1a455c9790
Max Rösch <github@elbebass.com>
2022-07-22 09:11:54 +0200

    [TASK] Adapt dependecies for latest TYPO3 major version

--- ext_emconf.php
+++ ext_emconf.php
@@ -4,26 +4,24 @@ $EM_CONF['static_info_tables_uk'] = [
     'title' => 'Static Info Tables (uk)',
     'description' => 'Ukrainian (uk) language pack for the Static Info Tables providing localized names for countries.',
     'category' => 'misc',
-    'version' => '6.9.2',
+    'version' => '7.0.0',
     'state' => 'stable',
-    'uploadfolder' => false,
-    'createDirs' => '',
-    'clearcacheonload' => false,
+    'clearCacheOnLoad' => 1,
     'author' => 'Max Rösch',
     'author_email' => 'dev@Leuchtfeuer.com',
     'author_company' => 'Leuchtfeuer Digital Marketing',
     'constraints' => [
         'depends' => [
-            'typo3' => '9.5.0-10.4.99',
+            'typo3' => '10.4.0-11.5.99',
             'php' => '7.2.0-7.4.99',
-            'static_info_tables' => '6.9.0-6.99.99',
+            'static_info_tables' => '6.7.3-11.5.99',
         ],
         'conflicts' => [],
         'suggests' => [],
     ],
     'autoload' => [
         'psr-4' => [
-            'Bitmotion\\StaticInfoTablesUk\\' => 'Classes'
-        ]
-    ]
+            'Bitmotion\\StaticInfoTablesUk\\' => 'Classes',
+        ],
+    ],
 ];
